422d5b5648deb7729130df42cde3006a94a447b2
Lloyd <lloyd@dreamatic.digital>
2019-08-07 18:25:51 +0200

    added deps

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function dr_enqueue(){
   $ver = DR_DEV_MODE ? time() : false;
 
   wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css', [], $ver );
-  wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [], $ver, true );
+  wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [ 'jquery' ], $ver, true );
 
   wp_enqueue_style( 'dr_styles' );
   wp_enqueue_script( 'dr_scripts' );
