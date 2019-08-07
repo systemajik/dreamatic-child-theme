eec756a3d4794960ab4014d69f0735f77e1ed7be
Lloyd <lloyd@dreamatic.digital>
2019-08-07 16:01:44 +0200

    Fixed WP register hook

--- functions.php
+++ functions.php
@@ -9,8 +9,8 @@ function dr_enqueue(){
   
   $uri = get_theme_file_uri();
 
-  wp_register_style( 'dr_styles' ), $uri . '/css/dreamatic.css' );
-  wp_register_script( 'dr_scripts' ), $uri . '/js/dreamatic.js', [], false, true );
+  wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css' );
+  wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [], false, true );
 
   wp_enqueue_style( 'dr_styles' );
   wp_enqueue_script( 'dr_scripts' );
