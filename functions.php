7e3b9457bd36684c7721cac5b2daf8e3366c0fc5
Lloyd <lloyd@dreamatic.digital>
2019-08-07 16:10:29 +0200

    Added dev mode param

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+
+//setup
+define( 'DR_DEV_MODE', true );
+
+
+
 //load shortcodes from individual files
 require_once(__DIR__ . '/shortcodes/dr_fusion_image_rollover.php');
 
@@ -8,13 +14,17 @@ require_once(__DIR__ . '/shortcodes/dr_fusion_image_rollover.php');
 function dr_enqueue(){
   
   $uri = get_theme_file_uri();
+  $ver = DR_DEV_MODE ? time() : false;
 
-  wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css' );
-  wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [], false, true );
+  wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css', [], $ver );
+  wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [], $ver, true );
 
   wp_enqueue_style( 'dr_styles' );
   wp_enqueue_script( 'dr_scripts' );
 }
 
+//hooks
+
+add_action( 'wp_enqueue_scripts', 'dr_enqueue' );
 
 
