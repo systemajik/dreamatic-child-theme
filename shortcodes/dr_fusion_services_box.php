8390a83ff66932f7158b28395bee5f7b964d195c
Lloyd <lloyd@dreamatic.digital>
2019-08-23 21:16:45 +0100

    css

--- shortcodes/dr_fusion_services_box.php
+++ shortcodes/dr_fusion_services_box.php
@@ -66,7 +66,7 @@ function dr_register_fusion_services_box() {
                   'dr-teal' => esc_attr__( 'Teal', 'fusion-builder' ),
                   'dr-amethyst'   => esc_attr__( 'Amethyst', 'fusion-builder' ),
                   'dr-amber'   => esc_attr__( 'Amber', 'fusion-builder' ),
-                  'dr-red'   => esc_attr__( 'Red', 'fusion-builder' ),
+                  'redness'   => esc_attr__( 'Red', 'fusion-builder' ),
                   'dr-blue'   => esc_attr__( 'Blue', 'fusion-builder' ),
                   'dr-green'   => esc_attr__( 'Green', 'fusion-builder' ),
                 ),
@@ -101,5 +101,5 @@ function dr_register_fusion_services_box() {
   add_action( 'fusion_builder_before_init', 'dr_register_fusion_services_box' );
 
 
-  
+
 //--------------------------------------------------------------
