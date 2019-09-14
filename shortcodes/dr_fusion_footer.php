b30c0a641643ae26cd009b59a5659f065268937c
Lloyd <lloyd@dreamatic.digital>
2019-09-14 17:26:38 +0200

    footer

--- shortcodes/dr_fusion_footer.php
+++ shortcodes/dr_fusion_footer.php
@@ -35,7 +35,7 @@ function dr_fusion_footer($atts, $content = null) {
     <div class="dr-copyright"> 
       <p class="dr-copyright-text"><?php echo $footertext; ?></p>
       <div class="dr-privacy">
-      <a href="/privacy" class="dr-footer-link">Privacy |</a><a href="/cookies" class="dr-footer-link"> Cookies </a>
+      <a href="/privacy-statement" class="dr-footer-link">Privacy |</a><a href="/cookie-statement" class="dr-footer-link"> Cookies </a>
       </div>
     
     </div>
