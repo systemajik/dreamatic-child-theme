78b4308d650ff0f33aa817919bfb373f0026d107
Lloyd <lloyd@dreamatic.digital>
2019-09-24 14:01:13 +0200

    functions

--- functions.php
+++ functions.php
@@ -29,9 +29,11 @@ function dr_enqueue(){
 
   wp_register_style( 'dr_styles', $uri . '/css/dreamatic.css', [], $ver );
   wp_register_script( 'dr_scripts', $uri . '/js/dreamatic.js', [ 'jquery' ], $ver, true );
+  wp_register_script( 'dr_typeform', 'https://public-assets.typeform.com/confab/embed.js', [], $ver, true);
 
   wp_enqueue_style( 'dr_styles' );
   wp_enqueue_script( 'dr_scripts' );
+  wp_enqueue_script( 'dr_typeform');
 }
 
 //hooks
